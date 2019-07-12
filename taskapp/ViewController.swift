2ba136f0596d3270cced722510dbee5e31ae59a9
樋川雄飛 <hikawayuuhi@hikawayuuhinomacbook-pro.local>
2019-07-12 11:30:49 +0900

    Initial Commit

---
+++ taskapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskapp
+//
+//  Created by 樋川雄飛 on 2019/07/12.
+//  Copyright © 2019 Yuhi Hikawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
